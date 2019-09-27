10a8577dd61a4caba9aace255d6fdc3622cd9fc3
laurenouellette <lauren.covey@sandag.org>
2019-09-27 11:22:13 -0700

    This commit fixed an issue with the column name in the final table. The source and final tables match now!

--- CTPP/2006-2010/QAQC/CTPP 2006-2010 Line Table QAQC.R
+++ CTPP/2006-2010/QAQC/CTPP 2006-2010 Line Table QAQC.R
@@ -9,30 +9,28 @@ source("..\\..\\..\\Common_functions\\readSQL.R")
 getwd()
 
 #Load source data
-file <- "R:/DPOE/CTPP/2012-2016/Documentation/2012-2016 CTPP Final Table Specs.xlsx"
-source1 <- read_excel(file, sheet = 'Table Specs')
+file <- "R:/DPOE/CTPP/2006-2010/Documentation/acs_2006thru2010_ctpp_table_shell.txt"
+source1 <- read.delim(file, header=TRUE, sep="|")
 
 #Load database data
 channel <- odbcDriverConnect('driver={SQL Server}; server=socioeca8; database=dpoe_stage; trusted_connection=true')
-sql_query1 <- 'SELECT * FROM dpoe_stage.dbo.ctpp_line'
+sql_query1 <- 'SELECT * FROM dpoe_stage.dbo.ctpp_2010_line'
 db1 <- sqlQuery(channel,sql_query1,stringsAsFactors = FALSE)
 odbcClose(channel)
 
 #Delete unneccessary columns
-source1 <- select(source1, -c("Wave","Type","Indent","Sub Range","1 Year Sourcing","3 Year Sourcing","5 Year Sourcing"))
+source1 <- select(source1, -"LINDENT")
 
-#Remove nas from source_data
-source1 <- source1[complete.cases(source1), ]
+#Rename source data
+source1 <- plyr::rename(source1, c("TBLID"="tblid","LINENO"="lineno","LDESC"="ldesc"))
 
 #Check data types
 str(source1)
 str(db1)
 
-#Convert to data frame
-source1 <- as.data.frame(source1)
-
-#Trim whitespace
-db1$Stub <- trimws(db1$Stub)
+#Convert data types
+source1$tblid <- as.character(source1$tblid)
+source1$ldesc <- as.character(source1$ldesc)
 
 #delete rownames for checking files match (R assigns arbitrary IDs)
 rownames(source1) <- NULL
@@ -50,7 +48,7 @@ which(source1!=db1, arr.ind = TRUE) #which command shows exactly which columns a
 
 #Load database data
 channel <- odbcDriverConnect('driver={SQL Server}; server=socioeca8; database=dpoe_stage; trusted_connection=true')
-sql_query1 <- 'SELECT * FROM dpoe_stage.dim.ctpp_line where yr = 2016'
+sql_query1 <- 'SELECT * FROM dpoe_stage.dim.ctpp_line where yr = 2010'
 final <- sqlQuery(channel,sql_query1,stringsAsFactors = FALSE)
 odbcClose(channel)
 
@@ -58,18 +56,11 @@ odbcClose(channel)
 final<- final[,-c(1:3)]
 
 #Rename source data
-source1 <- plyr::rename(source1, c("Table ID"="subj_table_id","Line Number"="line_number","Stub"="line_desc"))
+source1 <- plyr::rename(source1, c("tblid"="subject_table_id","lineno"="line_number","ldesc"="line_desc"))
 
 #Check data types
 str(source1)
 str(final)
-
-#Convert data types
-source1$line_number <- as.integer(source1$line_number)
-
-#Order table
-source1 <- source1[order(source1$subj_table_id,source1$line_number,source1$line_desc),]
-final <- final[order(final$subj_table_id,final$line_number,final$line_desc),]
 
 #delete rownames for checking files match (R assigns arbitrary IDs)
 rownames(source1) <- NULL
@@ -81,5 +72,3 @@ all.equal(source1,final) #check cell values and data types and will return the c
 identical(source1,final) #check cell values and data types
 which(source1!=final, arr.ind = TRUE) #which command shows exactly which columns are incorrect
 
-source1[1,1]
-final[1,1]
