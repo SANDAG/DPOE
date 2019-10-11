2da8e5c70fb327bc0a3a5df911cb60592e788758
laurenouellette <lauren.covey@sandag.org>
2019-10-10 18:07:08 -0700

    This commit adds new columns to the source files so that they can all be merged together.

--- Passenger Counting Program/2019/QAQC/Passenger Counting Program QAQC.R
+++ Passenger Counting Program/2019/QAQC/Passenger Counting Program QAQC.R
@@ -2,8 +2,8 @@
 
 #set working directory and access code to read in SQL queries
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("..\\..\\..\\..\\Common_functions\\Loading_in_packages.R")
-source("..\\..\\..\\..\\Common_functions\\readSQL.R")
+source("..\\..\\..\\Common_functions\\Loading_in_packages.R")
+source("..\\..\\..\\Common_functions\\readSQL.R")
 getwd()
 
 
@@ -39,25 +39,240 @@ source19 <- read_excel("R:\\DPOE\\Passenger Counting Program\\2019\\Source\\FY20
 #Load database data
 options(stringsAsFactors=FALSE)
 channel <- odbcDriverConnect('driver={SQL Server}; server=socioeca8; database=dpoe_stage; trusted_connection=true')
-sql_query <- 'SELECT * FROM dpoe_stage.XXXX'
+sql_query <- 'SELECT * FROM dpoe_stage.staging.passenger_counting_program'
 db <- sqlQuery(channel,sql_query,stringsAsFactors = FALSE)
 odbcClose(channel)
 
 #View column names in source data
-colnames(source)
+colnames(source02)
 colnames(db)
 colnames(source19)
+
+#Add new columns to individual source tables
+#Year
+source02$yr <- "2002"
+source03$yr <- "2003"
+source04$yr <- "2004"
+source05$yr <- "2005"
+source06$yr <- "2006"
+source07$yr <- "2007"
+source08$yr <- "2008"
+source09$yr <- "2009"
+source10$yr <- "2010"
+source11$yr <- "2011"
+source12$yr <- "2012"
+source13$yr <- "2013"
+source14$yr <- "2014"
+source15$yr <- "2015"
+source16$yr <- "2016"
+source17$yr <- "2017"
+source18$yr <- "2018"
+source19$yr <- "2019"
+
+#Date counted
+source08$date_counted <- NA
+source09$date_counted <- NA
+source10$date_counted <- NA
+source11$date_counted <- NA
+source12$date_counted <- NA
+source13$date_counted <- NA
+source14$date_counted <- NA
+source15$date_counted <- NA
+source16$date_counted <- NA
+source17$date_counted <- NA
+source18$date_counted <- NA
+source19$date_counted <- NA
+
+#Route name
+source03$route_name <- NA
+source04$route_name <- NA
+source05$route_name <- NA
+source06$route_name <- NA
+source07$route_name <- NA
+
+#Service code
+source02$service_code <- NA
+source03$service_code <- NA
+source04$service_code <- NA
+source05$service_code <- NA
+source06$service_code <- NA
+source07$service_code <- 
+  
+#Service type
+source02$service_type <- NA
+source03$service_type <- NA
+source04$service_type <- NA
+source05$service_type <- NA
+source06$service_type <- NA
+source07$service_type <- NA
+
+#Service mode
+source02$service_mode <- NA
+source03$service_mode <- NA
+source04$service_mode <- NA
+source05$service_mode <- NA
+source06$service_mode <- NA
+source07$service_mode <- NA
+
+#Service class
+source02$service_class <- NA
+source03$service_class <- NA
+source04$service_class <- NA
+source05$service_class <- NA
+source06$service_class <- NA
+source07$service_class <- NA
+
+#Trips Gross
+source02$trips_gross <- NA
+source03$trips_gross <- NA
+source04$trips_gross <- NA
+source05$trips_gross <- NA
+source06$trips_gross <- NA
+source07$trips_gross <- NA
+
+#sum_fon
+source02$sum_fon <- NA
+source03$sum_fon <- NA
+source04$sum_fon <- NA
+source05$sum_fon <- NA
+source06$sum_fon <- NA
+source07$sum_fon <- NA
+source08$sum_fon <- NA
+source09$sum_fon <- NA
+source10$sum_fon <- NA
+source11$sum_fon <- NA
+source12$sum_fon <- NA
+source13$sum_fon <- NA
+source14$sum_fon <- NA
+source15$sum_fon <- NA
+
+#sum_ron
+source02$sum_ron <- NA
+source03$sum_ron <- NA
+source04$sum_ron <- NA
+source05$sum_ron <- NA
+source06$sum_ron <- NA
+source07$sum_ron <- NA
+source08$sum_ron <- NA
+source09$sum_ron <- NA
+source10$sum_ron <- NA
+source11$sum_ron <- NA
+source12$sum_ron <- NA
+source13$sum_ron <- NA
+source14$sum_ron <- NA
+source15$sum_ron <- NA
+
+#sum_passengers_off
+source02$sum_passengers_off <- NA
+source03$sum_passengers_off <- NA
+source04$sum_passengers_off <- NA
+source05$sum_passengers_off <- NA
+source06$sum_passengers_off <- NA
+source07$sum_passengers_off <- NA
+
+#sum_foff
+source02$sum_foff <- NA
+source03$sum_foff <- NA
+source04$sum_foff <- NA
+source05$sum_foff <- NA
+source06$sum_foff <- NA
+source07$sum_foff <- NA
+source08$sum_foff <- NA
+source09$sum_foff <- NA
+source10$sum_foff <- NA
+source11$sum_foff <- NA
+source12$sum_foff <- NA
+source13$sum_foff <- NA
+source14$sum_foff <- NA
+source15$sum_foff <- NA
+
+#sum_roff
+source02$sum_roff <- NA
+source03$sum_roff <- NA
+source04$sum_roff <- NA
+source05$sum_roff <- NA
+source06$sum_roff <- NA
+source07$sum_roff <- NA
+source08$sum_roff <- NA
+source09$sum_roff <- NA
+source10$sum_roff <- NA
+source11$sum_roff <- NA
+source12$sum_roff <- NA
+source13$sum_roff <- NA
+source14$sum_roff <- NA
+source15$sum_roff <- NA
+
+#sum_wheelchairs
+source02$sum_wheelchairs <- NA
+source03$sum_wheelchairs <- NA
+source04$sum_wheelchairs <- NA
+source05$sum_wheelchairs <- NA
+source06$sum_wheelchairs <- NA
+source07$sum_wheelchairs <- NA
+source08$sum_wheelchairs <- NA
+source09$sum_wheelchairs <- NA
+
+#sum_bicycles
+source02$sum_bicycles <- NA
+source03$sum_bicycles <- NA
+source04$sum_bicycles <- NA
+source05$sum_bicycles <- NA
+source06$sum_bicycles <- NA
+source07$sum_bicycles <- NA
+source08$sum_bicycles <- NA
+source09$sum_bicycles <- NA
+source10$sum_bicycles <- NA
+source11$sum_bicycles <- NA
+source12$sum_bicycles <- NA
+source13$sum_bicycles <- NA
+source14$sum_bicycles <- NA
+source15$sum_bicycles <- NA
+source18$sum_bicycles <- NA
+
+#sum_kneels
+source02$sum_kneels <- NA
+source03$sum_kneels <- NA
+source04$sum_kneels <- NA
+source05$sum_kneels <- NA
+source06$sum_kneels <- NA
+source07$sum_kneels <- NA
+source08$sum_kneels <- NA
+source09$sum_kneels <- NA
+source10$sum_kneels <- NA
+source11$sum_kneels <- NA
+source12$sum_kneels <- NA
+source13$sum_kneels <- NA
+source14$sum_kneels <- NA
+source15$sum_kneels <- NA
+source16$sum_kneels <- NA
+source17$sum_kneels <- NA
+source18$sum_kneels <- NA
+source19$sum_kneels <- NA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Add new columns to source data table
+source$vintage_yr <- "2019"
 
 #Drop columns from source
 source <- select(source, -17:-42)
 
 #Rename source data. Make sure order is the same as in the database.
-source <- plyr::rename(source, c("V1"="timestamp", "V2"="station", "V3"="district", "V4"="route", "V5"="direction", "V6"="type", "V7"="seg_length", "V8"="samples", "V9"="observed", "V10"="total_flow", "V11"="delay35", "V12"="delay40", "V13"="delay45", "V14"="delay50", "V15"="delay55", "V16"="delay60"))
-
-# "ROUTE_NUMBER", "ROUTE_NAME", "SERVICE_CODE", "SERVICE_TYPE", "SERVICE_CLASS", "SERVICE_MODE", "TRIPS", "TRIPS_GROSS", "SUM_PASSENGERS_ON", "SUM_FON", "SUM_RON", "SUM_PASSENGERS_OFF", "SUM_FOFF", "SUM_ROFF",           
-# "SUM_WHEELCHAIRS", "SUM_BICYCLES", "SUM_KNEELS", "MAX_MAX_LOAD", "MAX_MAX_LOAD_P", "AVG_MAX_LOAD", "SUM_TP_EARLY", "SUM_TP_ONTIME", "SUM_TP_LATE", "ONTIME", "SUM_REVENUE_MILES", "SUM_REVENUE_HOURS", "AVG_PASSENGERS_ON", "AVG_PASSENGERS_OFF", 
-# "AVG_REVENUE_MILES", "AVG_REVENUE_HOURS", "SUM_PASSENGER_MILES", "AVG_PASSENGER_MILES", "SUM_SEAT_MILES",      "AVG_PASS_PER_MILE",   "AVG_PASS_PER_HOUR",  
-# "AVG_SEAT_MILES", "AVG_TRIP_LENGTH"    
+source <- plyr::rename(source02, c("F2"="date_counted", "[2002]"="route_num", "V3"="district", "V4"="route", "V5"="direction", "V6"="type", "V7"="seg_length", "V8"="samples", "V9"="observed", "V10"="total_flow", "V11"="delay35", "V12"="delay40", "V13"="delay45", "V14"="delay50", "V15"="delay55", "V16"="delay60"))
 
 #Check data types
 str(source)
